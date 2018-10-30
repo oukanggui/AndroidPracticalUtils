bcf748f69b386b64160d99020175b5a1b030379e
oukanggui <13802885143@139.com>
2018-10-30 20:52:33 +0800

    优化代码，减少if-else代码嵌套

--- utilslib/src/main/java/com/baymax/utilslib/TextUtil.java
+++ utilslib/src/main/java/com/baymax/utilslib/TextUtil.java
@@ -65,10 +65,9 @@ public class TextUtil {
         String regex = "^((13[0-9])|(14[5,7,9])|(15([0-3]|[5-9]))|(166)|(17[0,1,3,5,6,7,8])|(18[0-9])|(19[8|9]))\\d{8}$";
         if (strPhone.length() != 11) {
             return false;
-        } else {
-            Pattern p = Pattern.compile(regex);
-            Matcher m = p.matcher(strPhone);
-            return m.matches();
         }
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(strPhone);
+        return m.matches();
     }
 }
